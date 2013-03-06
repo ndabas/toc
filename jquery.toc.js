644b0a55c8d4771a9b278c1df216fefc022af622
Nikhil Dabas <nikhil@nikhildabas.com>
2013-03-07 05:21:40 +0530

    Refactoring code for convention over configuration

--- jquery.toc.js
+++ jquery.toc.js
@@ -22,54 +22,58 @@
 
     // Builds a list with the table of contents in the current selector.
     // contentContainer: where to look for headings
-    // options:
-    //    headings: string with a comma-separated list of selectors to be used as headings, in the
-    //    order which defines their relative heirarchy level
-    //    list: the tag used to create the lists, either "ul" or "ol"
-    $.fn.toc = function (contentContainer, options) {
+    // headings: string with a comma-separated list of selectors to be used as headings, in the
+    // order which defines their relative heirarchy level
+    $.fn.toc = function (contentContainer, headings) {
+        return this.each(function () {
+            // Defaults
+            contentContainer = contentContainer || "body";
+            headings = headings || "h1,h2,h3,h4";
 
-        // Defaults
-        contentContainer = contentContainer || "body";
-        options = $.extend({headings: "h1,h2,h3,h4", list: "ul"}, options);
+            // The upside-down stack keeps track of lists where new siblings or children may be
+            // added.
+            var stack = [$(this)],
+                listTag = this.tagName,
+                headingSelectors = headings.split(","),
+                currentLevel = 0;
 
-        // The upside-down stack keeps track of lists where new siblings or children may be added.
-        var stack = [this], headingSelectors = options.headings.split(","), currentLevel = 0;
+            // Set up some automatic IDs if we do not already have them
+            $(contentContainer).find(headings).attr("id", function (index, attr) {
+                // Generate a valid ID: must start with a letter, and contain only letters and
+                // numbers. All other characters are replaced with underscores.
+                return attr ||
+                    $(this).text().replace(/^[^A-Za-z]*/, "").replace(/[^A-Za-z0-9]+/g, "_");
+            }).each(function () {
+                // What level is the current heading?
+                var elem = $(this), level = $.map(headingSelectors, function (selector, index) {
+                    return elem.is(selector) ? index : undefined;
+                })[0];
 
-        // Set up some automatic IDs if we do not already have them
-        $(contentContainer).find(options.headings).attr("id", function (index, attr) {
-            // Generate a valid ID: must start with a letter, and contain only letters and numbers.
-            // All other characters are replaced with underscores.
-            return attr || $(this).text().replace(/^[^A-Za-z]*/, "").replace(/[^A-Za-z0-9]+/g, "_");
-        }).each(function () {
-            // What level is the current heading?
-            var elem = $(this), level = $.map(headingSelectors, function (selector, index) {
-                return elem.is(selector) ? index : undefined;
-            })[0] + 1;
+                if (level > currentLevel) {
+                    // If the heading is at a deeper level than where we are, start a new nested
+                    // list, but only if we already have some list items in the parent. If we do
+                    // not, that means that we're skipping levels, so we can just add new list items
+                    // at the current level.
+                    // In the upside-down stack, unshift = push, and stack[0] = the top.
+                    var parentItem = stack[0].children("li:last")[0];
+                    if (parentItem) {
+                        stack.unshift($("<" + listTag + "/>").appendTo(parentItem));
+                    }
+                } else {
+                    // Truncate the stack to the current level by chopping off the 'top' of the
+                    // stack. We also need to preserve at least one element in the stack - that is
+                    // the containing element.
+                    stack.splice(0, Math.min(currentLevel - level, Math.max(stack.length - 1, 0)));
+                }
 
-            if (level > currentLevel) {
-                // If the heading is at a deeper level than where we are, start a new nested list.
-                // if this list is to be a nested list, it must be a child of the last list item in
-                // the parent list.
-                // In the upside-down stack, unshift = push
-                stack.unshift($("<" + options.list + "/>").appendTo(
-                    stack[0].children("li:last")[0] || stack[0]
-                ));
-            } else {
-                // Truncate the stack to the current level by chopping off the 'top' of the stack.
-                // if there are more than two elements on the stack, preserve those - the first is
-                // the containing element, and the second is the 'root' list (ul/ol).
-                stack.splice(0, Math.min(currentLevel - level, Math.max(stack.length - 2, 0)));
-            }
+                // Add the list item
+                $("<li/>").appendTo(stack[0]).append(
+                    $("<a/>").text(elem.text()).attr("href", "#" + elem.attr("id"))
+                );
 
-            // Add the list item
-            $("<li/>").appendTo(stack[0]).append(
-                $("<a/>").text(elem.text()).attr("href", "#" + elem.attr("id"))
-            );
-
-            currentLevel = level;
+                currentLevel = level;
+            });
         });
-
-        return this;
     };
 
     $.fn.toc.noConflict = function () {
@@ -81,7 +85,7 @@
     $(window).on("load", function () {
         $("[data-toc]").each(function () {
             var elem = $(this), data = elem.data();
-            elem.toc(data.toc, {headings: data.tocHeadings, list: data.tocList});
+            elem.toc(data.toc, data.tocHeadings);
         });
     });
 }(window.jQuery));
